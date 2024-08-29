e87db39a73f4afc6560da377a9676db18a137c75
Joel <joel.chua.mingyi@gmail.com>
2024-08-29 10:32:54 +0800

    first commit

---
+++ src/test/java/com/example/javaswing/client/ChatClientTests.java
@@ -0,0 +1,13 @@
+package com.example.javaswing.client;
+
+import org.junit.jupiter.api.Test;
+
+public class ChatClientTests {
+
+    @Test
+    void testChatClientInitialization() {
+        // Basic test to ensure ChatClient initializes correctly
+        ChatClient client = new ChatClient("TestUser");
+        // Assertions can be added here to check initial state
+    }
+}
